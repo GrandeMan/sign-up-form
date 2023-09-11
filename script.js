8bb453d005e08dc5c002e6d895d1b3b9827c3285
Grandeman <joshuajmorales01@gmail.com>
2023-09-11 07:32:38 -0400

    Style page

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("#submit");
+const pwd = document.getElementById("#pwd");
+const confirm = document.getElementById("#pwd-confirm");
+
+
+
+button.addEventListener("click", () =>{
+
+})
+
